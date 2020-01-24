729d4a25afdfaa5d7b178c2636c3d4bffacefd1c
Michele <mvoto@cubeyou.com>
2020-01-24 23:05:48 +0100

    wip

--- simulation_config.py
+++ simulation_config.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+np.random.seed(42)
 circular_coords = [(12 + int(10*np.sin(2*t*np.pi)),12 + int(10*np.cos(2*t*np.pi))) for t in np.arange(0,1,0.1) ]
 random_coords = [(int(100*np.random.random()),int(100*np.random.random())) for _ in range(50)]
 AMENITIES_COORDINATES = random_coords#circular_coords
